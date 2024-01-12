0c8c4d64a5845d375f6503d79baa3dab089ceb09
Arnavgarg68 <arnavgarg221@gmail.com>
2024-01-12 14:38:28 +0530

    kisi trha quote aya

--- src/components/heading.js
+++ src/components/heading.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react'
 import './heading.css'
-export default function heading() {
-  return (
-    <div class="container">
-      <h1>To-Do</h1>
-    </div>
-  )
+export default function Heading() {
+  const [quote, setQuote] = useState('');
+  const quoteurl = "https://api.api-ninjas.com/v1/quotes";
+  const apikey = "sK7jt/zqnnq2hsgj+AQosQ==qUag6oj5lDLmmspe";
+  const fetchdata = async () => {
+    try {
+      const response = await fetch(quoteurl, {
+        method: 'GET',
+        headers: {
+          'X-Api-Key': apikey,
+          'Content-Type': 'application/json',
+        },
+      });
+    
+  if (response.ok) {
+      const result = await response.json();
+      console.log(result);
+      setQuote(result[0])
+    }
+    else {
+      console.error('Error: ', await response.text());
+    }
+  }
+  catch (error) {
+    console.error('Error:', error);}
+};
+useEffect(() => {
+  fetchdata(); // Call the fetchData function when the component mounts
+}, []);
+return (
+  <div className="container">
+    <h1>To-Do</h1>
+    <div className="quote">{quote.quote}</div>
+  </div>
+)
 }
