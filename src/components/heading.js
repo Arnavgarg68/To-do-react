3398da8782c3f2f420e355921645edaf0f6c7d91
Arnavgarg68 <arnavgarg221@gmail.com>
2024-01-12 15:33:23 +0530

    this is for ismounted

--- src/components/heading.js
+++ src/components/heading.js
@@ -1,38 +1,40 @@
-import React, { useEffect,useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './heading.css'
 export default function Heading() {
   const [quote, setQuote] = useState('');
+  const [mounted, setisMounted] = useState(true);
   const quoteurl = "https://api.api-ninjas.com/v1/quotes";
   const apikey = "sK7jt/zqnnq2hsgj+AQosQ==qUag6oj5lDLmmspe";
   const fetchdata = async () => {
     try {
       const response = await fetch(quoteurl, {
-        method: 'GET',
         headers: {
-          'X-Api-Key': apikey,
-          'Content-Type': 'application/json',
+          'X-Api-Key': apikey
         },
       });
-    
-  if (response.ok) {
-      const result = await response.json();
-      console.log(result);
-      setQuote(result[0])
+      if (response.ok&&mounted) {
+        const result = await response.json();
+        console.log(result);
+        setQuote(result[0])
+      }
+      else {
+        console.error('Error: ', await response.text());
+      }
     }
-    else {
-      console.error('Error: ', await response.text());
+    catch (error) {
+      console.error('Error:', error);
     }
-  }
-  catch (error) {
-    console.error('Error:', error);}
-};
-useEffect(() => {
-  fetchdata(); // Call the fetchData function when the component mounts
-}, []);
-return (
-  <div className="container">
-    <h1>To-Do</h1>
-    <div className="quote">{quote.quote}</div>
-  </div>
-)
+  };
+  useEffect(()=>{
+    fetchdata();
+
+    return ()=>{setisMounted(false);}
+  },[])
+  
+  return (
+    <div className="container">
+      <h1>To-Do</h1>
+      <div className="quote">{quote.quote}</div>
+    </div>
+  )
 }
