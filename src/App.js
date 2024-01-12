6ea393bc3c8a23dcdc624553333ffc54210dbd4b
Arnavgarg68 <arnavgarg221@gmail.com>
2024-01-13 02:40:11 +0530

    tasks counter completed

--- src/App.js
+++ src/App.js
@@ -2,15 +2,12 @@
 import Heading from "./components/Heading.js";
 import Task from "./components/Task.js";
 import './App.css'
-import {
-  BrowserRouter as Router,
-} from "react-router-dom";
 function App() {
   return (
-    <Router>
+    <>
       <Heading/>
       <Task/>
-    </Router>
+      </>
 
   )
 }
--- src/components/task.js
+++ src/components/task.js
@@ -3,26 +3,44 @@ import React, { useState } from 'react'
 // import Taskitem from './taskitem';
 export default function Task(props) {
     const [newtask,setNewTask]=useState('');
+    const [taskCount,setTaskCount]=useState(0);
     const [tasks,setTasks]=useState([]);
     const addtask=()=>{
         if(newtask.trim()!==''){
             setTasks([...tasks,{ id: Date.now(), text: newtask, completed: false }]);
             setNewTask('')
+            setTaskCount(taskCount+1)
         }
     }
     const updatetask=(taskId)=>{
-        setTasks(tasks.map(task => 
-            task.id === taskId ? { ...task, completed: !task.completed } : task
-          ));
+        // setTasks(tasks.map(task => 
+        //     task.id === taskId ? { ...task, completed: !task.completed } : task
+        //   ));
+        tasks.forEach(function(task,index){
+            if(task.id===taskId){
+                const uptask={...task,completed:!task.completed};
+                tasks[index]=uptask;
+                if(!task.completed){
+                    setTaskCount(taskCount-1);
+                }
+                else{
+                    setTaskCount(taskCount+1);
+                }
+                // task={...task, completed:!(task.completed)};
+            }
+            
+        })
     }
     const deleteTask = (taskId) => {
         setTasks(tasks.filter(task => task.id !== taskId));
+        setTaskCount(taskCount-1)
       }
   return (<>
     <div className="newtask">
       <input type="text" value={newtask} onChange={(e)=>setNewTask(e.target.value)} placeholder='add new task ..'/>
       <button onClick={addtask}>Add Task</button>
     </div>
+  <div className="taskscounter">Tasks remaining - {taskCount}</div>
     <div className="taskslist">
     <ul>
         {tasks.map(task=>{
